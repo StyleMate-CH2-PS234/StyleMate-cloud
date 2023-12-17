a2c1bea764373544734d107fdbcee50bdf6f7db0
shuyshuys <ivlyns@protonmail.com>
2023-12-17 21:31:26 +0700

    combine all route into one file

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const userRoutes = require('./users/routes');
+const upImgRoutes = require('./users/upImg');
+const modelRoutes = require('./users/model');
 
 const app = express();
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use('/users', userRoutes);
+app.use('/upload', upImgRoutes);
+app.use('/model', modelRoutes);
 
-app.listen(3000, () => {
-    console.log(`Listening on port 3000`);
-})
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'API Server Alive!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
