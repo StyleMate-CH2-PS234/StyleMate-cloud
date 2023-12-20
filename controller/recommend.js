1d6d59d4fe62aab56bae2ce1f936d2bc4554024a
shuyshuys <ivlyns@protonmail.com>
2023-12-21 01:53:40 +0700

    Add recommend endpoint to controller and routes

---
+++ controller/recommend.js
@@ -0,0 +1,38 @@
+const { Storage } = require('@google-cloud/storage');
+require('dotenv').config();
+
+const bucketName = process.env.GCS_BUCKET_NAME;
+
+const recommend = (req, res) => {
+  const faceClassification = req.query.face; // Get the face classification from the query parameters
+  const directoryPath = `${faceClassification}/`; // Construct the directory path
+
+    // Create a client
+    const storage = new Storage({
+        keyFilename: './config/cloud-storage.json'
+    });
+    const bucket = storage.bucket(bucketName);
+    bucket.getFiles({ prefix: directoryPath })
+    .then(([files]) => {
+      if (files.length === 0) {
+        res.status(404).json({ error: true, message: 'No files found' });
+      } else {
+        // Shuffle the array and take the first 10 items
+        const shuffledFiles = files.sort(() => 0.5 - Math.random());
+        const selectedFiles = shuffledFiles.slice(0, 10);
+    
+        // Construct the public URLs
+        const publicUrls = selectedFiles.map(file => `https://storage.googleapis.com/${bucketName}/${file.name}`);
+    
+        // Send the URLs in a JSON response
+        res.json({ error: false, imageUrls: publicUrls });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: true, message: err });
+    });
+};
+
+module.exports = {
+    recommend
+}
--- routes/model.js
+++ routes/model.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { loadModel, listModel } = require('../controller/controller');
+const { recommendModel } = require('../controller/recommend');
 const router = express.Router();
 
 router.get('/load', loadModel);
 router.get('/list', listModel);
+router.get('/recommend', recommendModel);
 
 module.exports = router;
