a648b798c2bb89c13176b663677658365e224ec0
shuyshuys <ivlyns@protonmail.com>
2023-12-21 16:48:38 +0700

    remove unused file

--- routes/barber.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getAllBarber, getBarberByQuery } = require('../controller/controller');
-const router = express.Router();
-
-router.get('/barber', getAllBarber);
-router.get('/barber/', getBarberByQuery);
-
-module.exports = router;
